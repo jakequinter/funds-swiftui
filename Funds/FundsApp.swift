93e0da43e3cad1822832cb31e26ed9f6dbd3db37
Jake Quinter <hello@jakequinter.io>
2022-07-08 16:58:30 -0400

    init

---
+++ Funds/FundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundsApp.swift
+//  Funds
+//
+//  Created by Jake Quinter on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
